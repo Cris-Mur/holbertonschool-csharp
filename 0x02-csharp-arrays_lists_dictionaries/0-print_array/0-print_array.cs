1cf8537ded92276f34be47ebe1b62066f831aed8
Cris-Mur <yomy98@gmail.com>
2020-12-03 11:43:50 -0500

    [[fix] zero case]

--- 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -8,15 +8,17 @@ class Array {
             return (null);
         }
         int[] array = new int[size];
-        for (int n = 0; n < size; n++)
-        {
-            array[n] = n;
-            if (n < (size - 1))
+        if (size > 0) {
+            for (int n = 0; n < size; n++)
             {
-                Console.Write("{0} ", n);
+                array[n] = n;
+                if (n < (size - 1))
+                {
+                    Console.Write("{0} ", n);
+                }
+                else
+                    Console.WriteLine(n);
             }
-            else
-                Console.WriteLine(n);
         }
         return (array);
     }
