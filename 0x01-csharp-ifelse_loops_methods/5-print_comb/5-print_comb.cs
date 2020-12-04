c3a440f9de43553d4c96c65ccfb90fc4fb624ff6
Cris-Mur <yomy98@gmail.com>
2020-12-04 11:39:16 -0500

    [[fix] print style]

--- 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -8,7 +8,7 @@ namespace _5_print_comb
         {
             for (int n = 0; n <= 99; n++) {
                 if (n < 99) {
-                    Console.Write("{0:d2}", n);
+                    Console.Write("{0:d2}, ", n);
                 } else {
                     Console.WriteLine("{0:d2}", n);
                 }
