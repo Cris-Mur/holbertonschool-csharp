8ef8cfda454450e1b21bbbcd62f6d8f159f4d09f
Cris-Mur <yomy98@gmail.com>
2020-12-04 11:52:55 -0500

    [[fix] print comb2]

--- 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -6,16 +6,16 @@ namespace _6_print_comb2
     {
         static void Main(string[] args)
         {
-            for (int n = 0; n < 10; n++)
-                for (int i = n + 1; i < 10; i++) {
-                    if (n != i) {
-                        Console.Write("{0:d}", n);
-                        if (n == 8) {
-                            Console.WriteLine("{0}", i);
-                        } else {
-                            Console.Write("{0:d}", i);
+            for (int n = 0; n <= 9; n++)
+                for (int i = n + 1; i <= 9; i++) {
+                    if (n != i && n < i) {
+                        Console.Write("{0}{1}", n, i);
+                        if (n != 8 || i != 9) {
+                            Console.Write(", ");
                         }
                     }
+                }
+            Console.WriteLine();
         }
     }
 }
