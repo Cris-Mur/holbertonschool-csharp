0ab025e70cb7d71ed8a07298d1045c770619ce5e
Cris-Mur <yomy98@gmail.com>
2020-12-04 16:45:56 -0500

    [[add] 11 multiply values in a dict]

---
+++ 0x02-csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary {
+    public static Dictionary<string, int> MultiplyBy2(Dictionary<string, int> myDict) {
+        Dictionary<string, int> mult = new Dictionary<string, int>();
+        foreach (var dict in myDict) {
+            mult[dict.Key] = dict.Value * 2;
+        }
+        return mult;
+    }
+}
