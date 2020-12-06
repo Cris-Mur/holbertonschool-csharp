0e4fe278fa8a9324053f650adcb8a1502f11cb13
Cris-Mur <yomy98@gmail.com>
2020-12-05 21:53:35 -0500

    [[add] 15 #D operations array]

---
+++ 0x02-csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Matrix {
+    public static int[,] Square(int[,] myMatrix) {
+        int [,] square = new int[myMatrix.GetLength(0),myMatrix.GetLength(1)];
+
+        for (int y = 0; y < myMatrix.GetLength(0); y++)
+            for (int x = 0; x < myMatrix.GetLength(1); x++)
+                square[y,x] = myMatrix[y,x] * myMatrix[y,x];
+
+        return square;
+    }
+}
