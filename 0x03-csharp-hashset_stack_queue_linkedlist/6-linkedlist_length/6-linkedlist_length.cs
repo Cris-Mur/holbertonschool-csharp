0954c9b27cbcc467c9f61ddc1b41bbd148678c33
Cris-Mur <yomy98@gmail.com>
2020-12-06 17:52:36 -0500

    [[add] 6 linked list length]

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList {
+    public static int Length(LinkedList<int> myLList) {
+        int ret = 0;
+        foreach (var count in myLList)
+            ret++;
+        return ret;
+    }
+}
