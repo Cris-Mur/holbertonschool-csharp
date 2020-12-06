f60917745eda493ab37ccde61ee4d6c2fdd2b611
Cris-Mur <yomy98@gmail.com>
2020-12-06 03:40:19 -0500

    [[add] 3 operations with stacks]

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack {
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search) {
+        Console.WriteLine($"Number of items: {aStack.Count}");
+        if (aStack.Count == 0)
+            Console.WriteLine("Stack is empty");
+        else
+            Console.WriteLine("Top item: {0}", aStack.Peek());
+        bool answ = aStack.Contains(search);
+        Console.WriteLine("Stack contains {0}: {1}", search, answ);
+        if (answ) {
+            while (aStack.Pop() != search) {
+                continue;
+            }
+        }
+        aStack.Push(newItem);
+        return aStack;
+    }
+}
