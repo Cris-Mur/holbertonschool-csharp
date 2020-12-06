ab468b5d148a8b5120b10c8e572502759c34ab7d
Cris-Mur <yomy98@gmail.com>
2020-12-06 00:54:02 -0500

    [[add] using hashset very usefull]

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List {
+    public static int Sum(List<int> myList) {
+        HashSet<int> list = new HashSet<int>(myList);
+        int ans = 0;
+        foreach (var value in list)
+            ans += value; 
+        return ans;
+    }
+}
