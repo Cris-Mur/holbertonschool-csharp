5d700f92c0ef3b7164e6032091d29c73713e8abf
Cris-Mur <yomy98@gmail.com>
2020-12-06 17:18:04 -0500

    [[add] 4 queue operations ]

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class  MyQueue {
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search) {
+        Console.WriteLine("Number of items: {0}", aQueue.Count);
+
+        if (aQueue.Count > 0) {
+            Console.WriteLine("First item {0}", aQueue.Peek());
+        } else {
+            Console.WriteLine("Queue is empty");
+        }
+        aQueue.Enqueue(newItem);
+
+        bool answ = aQueue.Contains(search);
+        Console.WriteLine("Queue contains {0}: {1}", search, answ);
+        if (answ) {
+            while (aQueue.Dequeue() != search)
+                continue;
+        }
+
+
+        return aQueue;
+    }
+}
