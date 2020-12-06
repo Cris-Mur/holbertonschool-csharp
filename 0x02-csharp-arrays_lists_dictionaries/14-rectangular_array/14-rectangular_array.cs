080710467451b4ce783822bf62abff672511a1f9
Cris-Mur <yomy98@gmail.com>
2020-12-05 20:06:35 -0500

    [[add] 14 create nD array]

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _14_rectangular_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [,] map = new int [5,5];
+            map[2,2] = 1;
+            for (int y = 0; y < 5; y++) {
+                for (int x = 0; x < 5; x++) {
+                    Console.Write(map[y,x]);
+                    if (x != 4)
+                        Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
