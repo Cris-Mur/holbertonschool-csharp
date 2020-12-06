72ba4e53a265418ccc0bca6705c726515ae1983f
Cris-Mur <yomy98@gmail.com>
2020-12-06 17:32:35 -0500

    [[add] 5 Linked list operations]

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList {
+    public static LinkedList<int> CreatePrint(int size) {
+        LinkedList<int> cosa = new LinkedList<int>();
+        if ((size > 0)) {
+            for (int sz = 0; sz < size; sz++) {
+                cosa.AddLast(sz);
+                Console.WriteLine(cosa.Last.Value);
+            }
+        }
+        return cosa;
+    }
+}
