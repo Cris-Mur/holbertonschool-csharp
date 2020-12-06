a130f8430111151d7b12e6fe03774782a020ddec
Cris-Mur <yomy98@gmail.com>
2020-12-06 01:32:01 -0500

    [[add] 2 not optimal algorithm but works :V]

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List {
+    public static List<int> DifferentElements(List<int> list1, List<int> list2) {
+        List<int> Nlist = new List<int>();
+        foreach (var value1 in list1)
+            foreach (var value2 in list2) {
+                if (!list2.Contains(value1) && !Nlist.Contains(value1)) {
+                    Nlist.Add(value1);
+                } else if (!list1.Contains(value2) && !Nlist.Contains(value2)) {
+                    Nlist.Add(value2);
+                }
+            }
+        Nlist.Sort();
+        return Nlist;
+    }
+}
