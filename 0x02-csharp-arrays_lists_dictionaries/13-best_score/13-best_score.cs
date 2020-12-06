6557c8f0157b34c8a7eb16cf78c2b3ce089f4e7f
Cris-Mur <yomy98@gmail.com>
2020-12-05 19:55:32 -0500

    [[add] 13 find max value]

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary {
+    public static string BestScore(Dictionary<string, int> myList) {
+        String name = "None";
+
+        if (myList.Count > 0) {
+            int best = 0;
+            foreach (KeyValuePair<string, int> row in myList)
+                if (best <= row.Value) {
+                    best = row.Value;
+                    name = row.Key;
+                }
+        }
+
+        return name;
+    }
+}
