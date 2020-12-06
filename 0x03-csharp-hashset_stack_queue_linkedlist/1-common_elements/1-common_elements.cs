9d082d642ed940c479fbcb339c83ed59e5e2d79f
Cris-Mur <yomy98@gmail.com>
2020-12-06 01:20:21 -0500

    [[add] 1 Contains]

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List{
+    public static List<int> CommonElements(List<int> list1, List<int> list2) {
+        List<int> Nlist = new List<int>();
+        foreach (var value in list1)
+            if (list2.Contains(value)) {
+                Nlist.Add(value);
+            }
+        return Nlist;
+    }
+}
