56c7aec2661baf2db623cc48f9e4e0e7c2a10739
Cris-Mur <yomy98@gmail.com>
2020-12-06 23:17:07 -0500

    [[add] 8 find value by scratch]

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList {
+    public static int FindNode(LinkedList<int> myLList, int value) {
+        int idx = 0;
+        foreach (var answ in myLList) {
+            if (answ == value) {
+                return idx;
+            } else
+                idx++;
+        }
+    idx = -1;
+        return idx;
+    }
+}
