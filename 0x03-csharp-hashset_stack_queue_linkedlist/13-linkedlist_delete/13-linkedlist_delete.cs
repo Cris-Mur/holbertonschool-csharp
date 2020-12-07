2fb79bd2535eea8b18fe3da381c87975e4b961ca
Cris-Mur <yomy98@gmail.com>
2020-12-06 23:47:55 -0500

    [[add] 13 delete at index]

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList {
+    public static void Delete(LinkedList<int> myLList, int index) {
+        LinkedListNode<int> node = myLList.First;
+        for (int idx = 0; idx < index; idx++)
+            node = node.Next;
+        myLList.Remove(node);
+    }
+}
