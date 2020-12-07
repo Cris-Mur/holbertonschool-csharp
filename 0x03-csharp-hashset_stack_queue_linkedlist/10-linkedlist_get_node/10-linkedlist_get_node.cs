981f21894e0f60f5cfa60605a1426981dd3ce972
Cris-Mur <yomy98@gmail.com>
2020-12-06 23:32:49 -0500

    [[add] 10 find node]

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList {
+    public static int GetNode(LinkedList<int> myLList, int n) {
+        if (myLList.Count >= n) {
+            int idx = 0;
+            foreach (var coso in myLList) {
+                if (idx == n)
+                    return coso;
+                idx++;
+            }
+        }
+        return 0;
+    }
+}
