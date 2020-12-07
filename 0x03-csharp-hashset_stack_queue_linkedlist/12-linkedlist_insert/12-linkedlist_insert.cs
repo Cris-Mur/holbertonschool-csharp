8f35792d3a696f9ccdc7ee57a32bb69802dcc895
Cris-Mur <yomy98@gmail.com>
2020-12-06 23:43:08 -0500

    [[add] 12 add in order]

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList {
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n) {
+        LinkedListNode<int> head = myLList.First;
+        while (head != null) {
+            if (head.Value > n)
+                return myLList.AddBefore(head, n);
+            head = head.Next;
+        }
+        return myLList.AddLast(n);
+    }
+}
